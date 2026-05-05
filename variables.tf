711208bb60af1725cf96a48dff21c1306a1abf17
rajasekhar burri <burriraja@gmail.com>
2026-05-05 13:08:00 +0530

    jenkins

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "rajasekhartech.site"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z00471191BK7Y747LU4X2"
+  description = "description"
+}
